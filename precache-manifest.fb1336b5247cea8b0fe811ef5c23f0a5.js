f29c60730e0c4b5f52c066767f7006c9575c30c7
randy <randz_argonillo@yahoo.com>
2019-08-03 22:55:57 +0800

    Updates

---
+++ precache-manifest.fb1336b5247cea8b0fe811ef5c23f0a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae1ea1a4622337066e899ca0fc240ad6",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "693e476f9f5db17d0a24",
+    "url": "/todo-app/static/css/2.a317ec87.chunk.css"
+  },
+  {
+    "revision": "00c03add0cb08c5fbc78",
+    "url": "/todo-app/static/css/main.5327f523.chunk.css"
+  },
+  {
+    "revision": "693e476f9f5db17d0a24",
+    "url": "/todo-app/static/js/2.78c3b32c.chunk.js"
+  },
+  {
+    "revision": "00c03add0cb08c5fbc78",
+    "url": "/todo-app/static/js/main.1d0c1e1b.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  }
+]);
